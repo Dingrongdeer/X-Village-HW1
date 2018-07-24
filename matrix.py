be35bbb1b80e176e98523aa13adc7eb058858bf7
Dingrongdeer <selena802@gmail.com>
2018-07-24 15:58:03 +0800

    add display

--- matrix.py
+++ matrix.py
@@ -3,28 +3,56 @@ import random
 from copy import deepcopy
 
 
-class Matrix:
+
+class Matrix():
 
     def __init__(self, nrows, ncols):
         """Construct a (nrows X ncols) matrix"""
-        pass
+        self.m = nrows
+        self.n = ncols
+        self.matrix = [[random.randint(0,9) for i in range(nrows)] for j in range(ncols)]
+ #       pass
 
     def add(self, m):
         """return a new Matrix object after summation"""
-        pass
+        self.new_matrix = [[random.randint(0,9) for i in range(self.m)] for j in range(self.n)]
+        for i in range(self.m):
+            for j in range(self.n):
+                print(self.matrix[i][j],end='  ')
+                
+            print("")
+        
+   #     pass
 
-    def sub(self, m):
-        """return a new Matrix object after substraction"""
-        pass
+    #def sub(self, m):
+    #    """return a new Matrix object after substraction"""
+    #    pass
 
-    def mul(self, m):
-        """return a new Matrix object after multiplication"""
-        pass
+    #def mul(self, m):
+    #    """return a new Matrix object after multiplication"""
+    #    pass
 
-    def transpose(self):
-        """return a new Matrix object after transpose"""
-        pass
+    #def transpose(self):
+    #    """return a new Matrix object after transpose"""
+    #    pass
     
     def display(self):
-        """Display the content in the matrix"""
-        pass
+    #    """Display the content in the matrix"""
+        for i in range(self.m):
+            for j in range(self.n):
+
+                print(self.matrix[i][j],end='  ')
+                
+            print("")
+    #    pass
+
+nrow = 3
+ncol = 3
+A = Matrix(nrow,ncol)
+B = Matrix(nrow,ncol)
+A.display()
+print(" "*5)
+B.display()
+
+A.add(B)
+
